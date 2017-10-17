7fa208b0e32622b58d5487776304a8196f699707
Kuzmin <kuzmin@amazon.com>
2017-10-17 09:47:47 +0200

    Remove newline from input prompts

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,14 +4,14 @@
 # Function implements the "guessinggame" logic.
 function guessinggame {
     local items_total=$(ls | wc -l)
-    echo "Try to guess the correct number of files in this directory: "
+    echo -n "Try to guess the correct number of files in this directory: "
     read input
     while [[ ! $input -eq $items_total ]]; do
 	if [[ $input -lt $items_total ]]; then
-	    echo "Your guess is LESS then the right one. Try again: "
+	    echo -n "Your guess is LESS then the right one. Try again: "
 	    read input
 	else
-	    echo "Your guess is GREATER then the right one. Try again: "
+	    echo -n "Your guess is GREATER then the right one. Try again: "
 	    read input
 	fi
     done
